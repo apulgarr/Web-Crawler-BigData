2318aa68f1cceda6bd7f9514f20e2011b93c4325
Andres Pulgarin <master@Schuyler-DeBargers-iMac-3.local>
2018-09-30 19:50:38 -0500

    Limpiando scripts

--- datos_siata/siata_scraping.py
+++ datos_siata/siata_scraping.py
@@ -12,17 +12,6 @@ class SiataSpider(scrapy.Spider):
     def parse(self, response):
         url = 'https://siata.gov.co/descarga_siata//application/assets/assets-siata/downloads/HgMfhJBrrVvecoZ2TbTeJg/'
         urls = response.xpath('//a/@href').extract()[5:]
-        print len(urls)
-        #instance = Data(response.url, urls)
-        #instance.get_item()
-        #for csv in response.xpath('//a/@href').extract()[5:]:
-            #url_name = url + csv
-            #self.csv = csv
-            #print self.csv, 'scrapy script'
-            #yield scrapy.Request(url_name, callback=self.parse_another)
-
-
-    #def parse_another(self, response):
-        #instance = Data(response.url, self.csv)
-        #data = instance.get_item()
+        instance = Data(response.url, urls)
+        instance.get_item()
 
